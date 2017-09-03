a77b3167a6c9b8b296554591171f5c6219f1c35f
Aaron Watson <aaron@Aarons-MacBook.local>
2017-09-02 20:27:19 -0400

    added small thumbnail example

--- examples/run.sh
+++ examples/run.sh
@@ -6,6 +6,8 @@ echo "Searching for thumbnail..."
 ../comphisto.py -d dataset/ -q query/thumbnail.JPG
 echo "Searching for sharpened thumbnail..."
 ../comphisto.py -d dataset/ -q query/thumbnail_sharpen.JPG
+echo "Searching for small thumbnail..."
+../comphisto.py -d dataset/ -q query/thumbnail_small.JPG
 echo "Searching for b&w thumbnail..."
 ../comphisto.py -d dataset/ -q query/thumbnail_bw.JPG
 echo "Searching for b&w thumbnail colorblind..."
